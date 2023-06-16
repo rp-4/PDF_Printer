8c47f83afa7bdae704b3c38857eeaaccd55bfed4
Rinkesh P <rinkeshpatel44@gmail.com>
2023-06-16 10:01:55 -0400

    Update main.py
    - added Test environment
    - adding a confirmation to print the file or not
    - changed algo of printing single page at a time to all page together
        - it will detect all pages and store in a dict
        - then send to printer function and it will print all pages together
        - single pdf at a time.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ st = time.time()
 
 def main():
     
-    allPrinters = [r"Brother HL-L5200DW series Printer",r"HP Color LaserJet Pro M478f-9f [D39EC8]"]
+    allPrinters = [r"Normal Printer Name",r"Color Printer Name"]
     
     for printer in allPrinters:
         checkPrinter(printer)
@@ -33,9 +33,21 @@ def main():
     #ask for folder location
     path = input("Enter folder Path: ").strip()
     
+    env = input("Test = 0 and Print = 1 :").strip()
+    
+    if env == "0":
+        t = "0"
+    elif env == "1":
+        t = "1"
+        
     pdfs = getAllPdfUrls(path)
     for p in pdfs:
-        checkPDFpageColor(p)
+        print("\nPrinting==========: "+str(p).rsplit("\\",1)[-1]+"\n")
+        ans = input("Want to print, 1 for Yes & 0 for No, x to cancel :")
+        if ans == "1":
+            checkPDFpageColor(p, t)
+        elif ans == "x":
+            sys.exit("Thank you for using the script, see you nest time !!")
         
     
     
@@ -53,41 +65,79 @@ def getAllPdfUrls(path):
     #return all full url of pdf files except VC
     all_PDFs = []
     d = pathlib.Path(path)
-    for i in d.iterdir():
-        if i.is_file() and str(i).endswith("-VC.PDF"):
-            #print("VC")
-            pass
-        elif i.is_file() and str(i).endswith(".PDF"):
-            #print(i)
-            all_PDFs.append(i)
+    try:
+        for i in d.iterdir():
+            if i.is_file() and str(i).count("-VC") > 1:
+                #print("VC")
+                pass
+            elif i.is_file() and str(i).endswith(".pdf") and len(str(i).rsplit("\\",1)[-1]) < 9:
+                #print(len(str(i).rsplit("\\",1)[-1]))
+                pass
+            elif i.is_file() and (str(i).endswith(".PDF") or str(i).endswith(".pdf")) and len(str(i).rsplit("\\",1)[-1]) > 5:
+                #print(i)
+                #print(len(str(i).rsplit("\\",1)[-1]))
+                all_PDFs.append(i)
+    except:    
+        sys.exit("No single pdf found.")
     
     #print(all_PDFs)
     return all_PDFs
                 
 
-def checkPDFpageColor(path):
+def checkPDFpageColor(path, envmnt):
     print("==============================================")
-    print(path)
-    images = convert_from_path(path,poppler_path=r'path to \poppler-23.05.0\Library\bin')
+    #print(path)
+    images = convert_from_path(path,poppler_path=r'Path to \poppler-23.05.0\Library\bin')
     
     p = 1
     npg = []
     cpg = []
+    normalPagesToPrint = {}
+    colorPagesToPrint = {}
+    
+    #variable for printers to use in dict
+    test = "Microsoft Print to PDF"
+    if envmnt == "0":
+        color = test
+        normal = test
+    elif envmnt == "1":
+        color = "Color Printer Name"
+        normal = "Normal Printer Name"
+    
+    
     for image in images:
         img = np.array(image.convert('HSV'))
         hsv_sum = img.sum(0).sum(0)
         if hsv_sum[0] == 0 and hsv_sum[1] == 0:
-            print(p, "====== Normal")
-            printSinglePage(path, "default", p, "Black and White Printer Name")
+            #print(p, "====== Normal")
+            #printSinglePage(path, "default", p, "Microsoft Print to PDF")
+            #printSinglePage(path, "default", p, "Normal Printer Name")
             npg.append(p)
             p += 1
         else:
-            print(p, "====== Color")
-            printSinglePage(path,"default", p, "Color Printer Name")
+            #print(p, "====== Color")
+            #printSinglePage(path,"default", p, "Microsoft Print to PDF")
+            #printSinglePage(path,"default", p, "Color Printer Name")
             cpg.append(p)
             p += 1
-    print(npg)
-    print(cpg)
+    #print(npg)
+    normalPagesToPrint.update({"Path":f"{path}",
+        "Pages": npg,
+        "Printer": normal})
+    #print(normalPagesToPrint)
+    #print(cpg)
+    colorPagesToPrint.update({"Path":f"{path}",
+        "Pages": cpg,
+        "Printer": color})
+    #print(colorPagesToPrint)
+    
+    if normalPagesToPrint["Pages"]:
+        printWholePDF(normalPagesToPrint["Path"], "default", str(normalPagesToPrint["Pages"])[1:-1], normalPagesToPrint["Printer"])
+    if colorPagesToPrint["Pages"]:
+        printWholePDF(colorPagesToPrint["Path"], "default", str(colorPagesToPrint["Pages"])[1:-1], colorPagesToPrint["Printer"])
+    
+
+#optional function to ask for printer and save in a file which can be used to read printer name
 
 def askForPrinterName():
     color = input("Enter Color Printer Name: ").strip()
@@ -97,43 +147,11 @@ def askForPrinterName():
     with open("0.txt", "w+") as file:
         for i in contents:
             file.write(str(i)+"\n")
-            
-
-def printIt(fileURL, printerColor):
-    
-    sumatra = "path to \SumatraPDF-3.4.6-64.exe"
-    
-    # opened file as reading (r) in binary (b) mode
-    file = open(fileURL, 'rb')
-
-    # store data in pdfReader
-    pdfReader = PyPDF2.PdfReader(file)
-
-    # count number of pages
-    totalPages = len(pdfReader.pages)
-
-    # print number of pages
-    print(f"Total Pages: {totalPages}")
-        
-    
-    p = [1,3]
-    
-    pageList = str(p)[1:-1]
-    
-    pages = f"landscape, {pageList},1x"
-            
-  #try to print the pdf and if doesnt work then give error
-    try:
-        
-        subprocess.call([sumatra, '-print-to', "Microsoft Print to PDF", '-print-settings', pages, pdf_name])
-
-    except BaseException as msg:
-        print(msg)
         
         
-def printSinglePage(fileURL, orientation, pg, printerColor):
+def printWholePDF(fileURL, orientation, pg, printerColor):
     
-    sumatra = "path to \\SumatraPDF-3.4.6-64.exe"
+    sumatra = "Path to \\SumatraPDF-3.4.6-64\\SumatraPDF-3.4.6-64.exe"
     
     # opened file as reading (r) in binary (b) mode
     file = open(fileURL, 'rb')
